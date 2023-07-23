d9be06b33ecec29de2a8750eb47e12c3d47c7375
Martin Lozano <85972433+Lazaro995@users.noreply.github.com>
2023-07-23 17:05:54 -0300

    addition of functions and change of direction of buttons +1 and -1 (#4)

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -1,21 +1,45 @@
 import { Button, Text, Grid } from "@nextui-org/react";
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { CounterProps } from "./interfaces";
 
-const Counter: FC<CounterProps> = ({ available, stock, imagen }) => {
+const Counter: FC<CounterProps> = ({ available, stock, imagen, price, quantity }) => {
   const [value, setValue] = useState(0);
+  const [currentPrice, setCurrentPrice] = useState(0);
+  const [originalPrice, setOriginalPrice] = useState(0);
+
+  useEffect(() => {
+    setCurrentPrice(0);
+    setOriginalPrice(0);
+  }, [price, quantity]);
+
+  useEffect(() => {
+    handlePriceChange();
+  }, [currentPrice]);
 
   const writeValue = (newValue: number) => {
     setValue(value + newValue);
+    setCurrentPrice((currentPrice + newValue * price) >= 0 ? (currentPrice + newValue * price) : 0);
   };
+
+  const handlePriceChange = () => {
+  };
+
+  const handleReset = () => {
+    setValue(0);
+    setCurrentPrice(originalPrice);
+  };
+if(stock)
 
   return (
     <Grid.Container>
-      <img src={imagen} alt="" />
+      <img src={imagen} alt="this is a img" />
+      <Text h3 size={20} weight="bold">
+        Price: ${currentPrice}
+      </Text>
       <Grid.Container gap={2} justify="center">
-        {!available && "No ta disponible XD"}
+        {!available}
         <Grid xs={3}>
-          <Button size="xs" onPress={() => writeValue(+1)}>
+          <Button size="xs" onPress={() => writeValue(-1)}>
             {" "}
             -{" "}
           </Button>
@@ -37,11 +61,11 @@ const Counter: FC<CounterProps> = ({ available, stock, imagen }) => {
           </Text>
         </Grid>
         <Grid xs={3}>
-          <Button size="xs" onPress={() => writeValue(-1)}>
+          <Button size="xs" onPress={() => writeValue(+1)}>
             +
           </Button>
         </Grid>
-        <Button onPress={() => writeValue(-value)}>Reset</Button>
+        <Button onPress={handleReset}>Reset</Button> {/* Utilizamos handleReset para restablecer el precio */}
       </Grid.Container>
     </Grid.Container>
   );
--- src/components/counter/interfaces.ts
+++ src/components/counter/interfaces.ts
@@ -2,4 +2,6 @@ export interface CounterProps {
     stock: number;
     available: boolean;
     imagen: string;
+    price: number;
+    quantity: number;
 }
