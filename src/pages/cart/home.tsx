77291c057d07b911bcb05bb7c7657e93940d498a
Martin Lozano <martin.lozano995@gmail.com>
2023-08-03 08:49:59 -0300

    private

--- src/pages/cart/home.tsx
+++ src/pages/cart/home.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { CarouselHome } from '../../components/carouselHome';
-import Profile from './profile';
 
 function Home() {
   return (
     <div><p>This is the home page</p>
       <Link to='/cart'>GA</Link>
-      <Profile />
       <CarouselHome />
     </div>
   )
